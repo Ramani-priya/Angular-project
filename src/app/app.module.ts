e40178d41ac72b55bda4fc680aa54f6a7518ff27
Ramani Priya Kolluru <168r1a04k4@cmrec.ac.in>
2020-06-07 20:07:15 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { baseURL } from './shared/baseurl';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
